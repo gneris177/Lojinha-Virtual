8fef96ec43cec9e66d41c4ec453532c9f6109af4
Your Name <gneris177@gmail.com>
2021-07-18 20:47:46 -0300

    add icons

--- components/Footer/Footer.Styles.js
+++ components/Footer/Footer.Styles.js
@@ -4,12 +4,14 @@ import { colors, metrics } from "../styles";
 //icons
 import { YoutubeWithCircle } from "@styled-icons/entypo-social/YoutubeWithCircle";
 import { TwitterWithCircle } from "@styled-icons/entypo-social/TwitterWithCircle";
-import { Motorcycle } from "@styled-icons/fa-solid/Motorcycle";
+import { InstagramWithCircle } from "@styled-icons/entypo-social/InstagramWithCircle";
+import { Facebook } from "@styled-icons/bootstrap/Facebook";
 
 export const Wrapper = styled.footer`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  align-items: center;
   padding: 2%;
   background: ${colors.bgSecundary};
   border-top: 5px solid #b41c8b;
@@ -32,16 +34,22 @@ export const Link = styled.a`
   color: #b41c8b;
 `;
 
-
-
-
 export const IconYt = styled(YoutubeWithCircle)`
-  color: #FF0000 ;
+  color: #ff0000;
   width: 30px;
 `;
 
 export const IconTwitter = styled(TwitterWithCircle)`
-  color: #43BCEB;
+  color: #43bceb;
   width: 30px;
 `;
 
+export const IconInsta = styled(InstagramWithCircle)`
+  color: linear-gradient(180deg, #D8307B 0%, #F39259 100%);
+  width: 30px;
+`;
+
+export const IconFb = styled(Facebook)`
+  color: #1877F2;
+  width: 30px;
+`;
--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,14 +1,18 @@
 import * as S from "./Footer.Styles";
-
+import Image from "next/image";
 //components
 import Logo from "../Logo";
 import Title from "../Title";
-import Text from "../Text";
-import Cart from "../Cart";
 
 const Footer = () => (
   <S.Wrapper>
-    <figure></figure>
+    <div>
+      <S.Link href="">Políticas de Privacidade</S.Link>
+      <figure>
+        <Image src="/img/softcom-logo.png" width={190} height={50} />
+      </figure>
+    </div>
+
     <div>
       <S.Link href="">Políticas de Privacidade</S.Link>
     </div>
@@ -19,12 +23,17 @@ const Footer = () => (
       <S.Link>Nossas redes sociais</S.Link>
       <div>
         <S.Link href="">
+          <S.IconFb />
+        </S.Link>
+        <S.Link href="">
+          <S.IconInsta />
+        </S.Link>
+        <S.Link href="">
           <S.IconTwitter />
         </S.Link>
-        <S.Link  href="">
+        <S.Link href="">
           <S.IconYt />
         </S.Link>
-        <S.Link  href=""></S.Link>
       </div>
     </div>
   </S.Wrapper>
